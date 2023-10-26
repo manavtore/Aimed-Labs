dfed4647fef5c0bca95015f901f0ba82238fb755
manavtore <toremanav@gmail.com>
2023-10-27 00:47:23 +0530

    refactor: restructure folders and rename app to Aimed-Labs

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:aimed_labs/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const AimedLabs());
+    await tester.pumpWidget(const Aimed_Labs());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
